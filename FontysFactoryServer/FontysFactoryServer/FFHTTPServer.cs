09467be6dc394d1cf7ce877ce00aedeba18d9145
Wouter Habets <wouterhabets@gmail.com>
2014-09-24 15:50:34 +0200

    Added HttpServer

---
+++ FontysFactoryServer/FontysFactoryServer/FFHTTPServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace FontysFactoryServer
+{
+    public class FFHTTPServer : HttpServer
+    {
+        public FFHTTPServer(int port)
+            : base(port) {
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            Console.WriteLine("POST request: {0}", p.http_url);
+            string data = inputData.ReadToEnd();
+
+            p.writeSuccess();
+            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+            Console.WriteLine("hasdfasdfasdfa");
+
+
+        }
+    }
+}
---
+++ FontysFactoryServer/FontysFactoryServer/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FontysFactoryServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HttpServer httpServer;
+            httpServer = new FFHTTPServer(8080);
+            Thread thread = new Thread(new ThreadStart(httpServer.listen));
+            thread.Start();
+        }
+    }
+}
