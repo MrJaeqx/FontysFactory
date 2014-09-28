1c3414569c8c9cf7ab169fc3883ab4b79ac905cd
OttkO <adrian_iankov@abv.bg>
2014-09-28 13:36:37 +0200

    Server + Console
    Server handles xml through post and client to send the file to test.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace Examples.System.Net
+{
+    public class WebRequestPostExample
+    {
+        public static void Main()
+        {
+            // Create a request using a URL that can receive a post. 
+            WebRequest request = WebRequest.Create("http://localhost:8080/");
+            // Set the Method property of the request to POST.
+            request.Method = "POST";
+            // Create POST data and convert it to a byte array.
+            XmlDocument dom = new XmlDocument();
+            dom.Load("Test.xml");
+
+            byte[] byteArray = Encoding.Default.GetBytes(dom.OuterXml);
+            // Set the ContentType property of the WebRequest.
+            request.ContentType = "application/x-www-form-urlencoded";
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+            // Get the request stream.
+            Stream dataStream = request.GetRequestStream();
+            // Write the data to the request stream.
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            // Close the Stream object.
+            dataStream.Close();
+            // Get the response.
+                WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+            Console.ReadLine();
+            // Clean up the streams.
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+    }
+}
--- HttpServerExample/SimpleHttpServer/SimpleHttpServer.cs
+++ HttpServerExample/SimpleHttpServer/SimpleHttpServer.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Xml;
 
 // offered to the public domain for any use with no restriction
 // and also with no warranty of any kind, please enjoy. - David Jeske. 
@@ -225,11 +226,17 @@ namespace Bend.Util {
         public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData) {
             Console.WriteLine("POST request: {0}", p.http_url);
             string data = inputData.ReadToEnd();
+            XmlDocument Doc = new XmlDocument();
+            Doc.LoadXml(data);
+            XmlNodeList elemList = Doc.GetElementsByTagName("title");
+            Console.WriteLine(elemList[0].InnerXml);
+
 
             p.writeSuccess();
-            p.outputStream.WriteLine("<html><body><h1>test server</h1>");
-            p.outputStream.WriteLine("<a href=/test>return</a><p>");
-            p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+            //p.outputStream.WriteLine("<html><body><h1>test server</h1>");
+            //p.outputStream.WriteLine("<a href=/test>return</a><p>");
+            //p.outputStream.WriteLine("postbody: <pre>{0}</pre>", data);
+            p.outputStream.WriteLine("Accept");
             
 
         }
